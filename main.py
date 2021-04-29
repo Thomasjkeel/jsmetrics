ea03424d9e2040d0c8a049fe3aabfbe14a501ae5
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-04-29 10:50:39 +0100

    add data arg

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ ALL_EXPERIMENTS = {'experiment1':test_uvwind.main}
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run an experiment.')
-    parser.add_argument('--experiment', type=str, help='path to a python script')
+    parser.add_argument('-e','--experiment', type=str, help='path to a python script')
+    parser.add_argument('-d', '--data', type=str, help='path to a netcdf4 data')
     args = parser.parse_args()
     print("Running script: ", args.experiment)
 
+    ## TODO: make error handling smarter
     try:
         ALL_EXPERIMENTS[args.experiment]()
     except Exception as e:

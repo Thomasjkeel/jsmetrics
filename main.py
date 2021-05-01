f372326b4dc57a3db3b7e00018c1e55c35c36fc5
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-05-01 11:59:29 +0100

    make bash interface friendlier

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import argparse
 from experiments import get_available_metrics
 
-ALL_EXPERIMENTS = {'test_metrics':get_available_metrics.main}
+ALL_EXPERIMENTS = {'get_available_metrics':{"description":"Will print out all metrics available for this dataset","script":get_available_metrics.main}}
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run an experiment.')
     parser.add_argument('-e','--experiment', type=str, help='path to a python script')
     parser.add_argument('-d', '--data', type=str, help='path to a netcdf4 data')
+    parser.add_argument('-m', '--metrics', type=tuple, help='names of metrics to use')
+    parser.add_argument('-l', '--ls', help='list all experiments', action='store_true')
     args = parser.parse_args()
-    print("Running script: ", args.experiment)
+
+    if args.ls:
+        print("\nAll experiments:\n")
+        print("-"*20)
+        for exp in ALL_EXPERIMENTS.keys():
+            print(exp, "—", ALL_EXPERIMENTS[exp]["description"])
+        print("-"*20)
+        print("\n")
 
     ## TODO: make error handling smarter
-    if args.experiment not in ALL_EXPERIMENTS.keys():
-        print(args.experiment, 'is not a known experiment')
+    if not args.experiment:
+        print('Error: No experiment declared. Use: \'-e experiment_name\'')
+    elif args.experiment not in ALL_EXPERIMENTS.keys():
+        print('Error: \'%s\' is not a known experiment. Run \'-l\' for list of experiments' % (args.experiment))
     else:
-        ALL_EXPERIMENTS[args.experiment](args.data)
+        print("Running script: ", args.experiment)
+        print("Metrics to use: ", args.metrics)
+        ALL_EXPERIMENTS[args.experiment]["script"](args.data)
         

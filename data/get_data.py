5108b3248977c69d5247ea4357ee60284a6e3b54
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-05-04 13:20:45 +0100

    add get_data

---
+++ data/get_data.py
@@ -0,0 +1,20 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+## For 150 - 950 hPa:
+# p_levels = [str(z)  for z in ([1] + list(range(150, 1000, 50)))]
+p_levels = [700, 850, 925]
+
+c.retrieve("reanalysis-era5-pressure-levels", 
+    {
+    "variable":"u",
+    "date":"1979-01-01/2020-03-01",
+    "time": "00:00",
+    "pressure_level": p_levels,
+    "area": [75, -360, 15, -0],
+    "product_type": "reanalysis",
+    "format": 'netcdf',
+    "grid": "1.0/1.0"
+    },
+"era5_u_wind_99to21.nc")

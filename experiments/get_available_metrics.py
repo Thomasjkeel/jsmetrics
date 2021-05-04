e09c0ea53dadf01c5423c7da0b7b03d149333377
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-05-04 11:34:25 +0100

    change return coord error to true

--- experiments/get_available_metrics.py
+++ experiments/get_available_metrics.py
@@ -11,6 +11,6 @@ def main(data_path):
     UKESM1_SSP585 = xr.merge([UKESM1_SSP585_U, UKESM1_SSP585_V])
     ukesm1_ssp585 = data_formatter.DataFormatter(UKESM1_SSP585)
     ukesm1_ssp585 = ukesm1_ssp585.subset(lat=slice(0, 90)) # , plev=50000)
-    ukesm1_ssp585.get_available_metrics(all_metrics, return_coord_error=False)
+    ukesm1_ssp585.get_available_metrics(all_metrics, return_coord_error=True)
     return 
 

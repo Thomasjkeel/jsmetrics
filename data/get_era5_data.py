386294463a3d84cdf26790993cf7a4c34fd11a79
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-05-05 12:56:16 +0100

    add more plevs

--- data/get_era5_data.py
+++ data/get_era5_data.py
@@ -2,9 +2,7 @@ import cdsapi
 
 c = cdsapi.Client()
 
-## For 150 - 950 hPa:
-# p_levels = [str(z)  for z in ([1] + list(range(150, 1000, 50)))]
-p_levels = [700, 850, 925]
+p_levels = [250, 300, 500, 700, 850, 925]
 
 c.retrieve("reanalysis-era5-pressure-levels", 
     {
@@ -12,7 +10,7 @@ c.retrieve("reanalysis-era5-pressure-levels",
     "date":"1979-01-01/2020-03-01",
     "time": "00:00",
     "pressure_level": p_levels,
-    "area": [75, -360, 15, -0],
+    "area": [90, -360, 0, -0],
     "product_type": "reanalysis",
     "format": 'netcdf',
     "grid": "1.0/1.0"

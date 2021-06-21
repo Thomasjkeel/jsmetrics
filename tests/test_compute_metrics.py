e33e394a19c5b74a28cef81fbc81ffb62ba17f5a
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-06-21 15:24:37 +0100

    add tests

---
+++ tests/test_compute_metrics.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+# class MyTest(unittest.TestCase):
+#     def setUp(self):
+#         return
+
+#     def test_subset(self):
+#         self.assertEqual(fun(3), 4)
+    
+#     # def test
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,29 +1,36 @@
 from metrics import compute_metrics
 from metrics.jetstream_metrics_dict import JETSTREAM_METRIC_DICT
+import numpy as np
 import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import unittest
 
-def main(data_path, **kwargs):
-    print("Starting!")
-    all_metrics = JETSTREAM_METRIC_DICT
-    data_dir = 'data/'
-    UKESM1_SSP585_U = xr.open_dataset(data_dir + "ua_day_UKESM1-0-LL_ssp585_r2i1p1f2_gn_20150101-20491230.nc")
-    UKESM1_SSP585_V = xr.open_dataset(data_dir + "va_day_UKESM1-0-LL_ssp585_r2i1p1f2_gn_20150101-20491230.nc")
-    UKESM1_SSP585 = xr.merge([UKESM1_SSP585_U, UKESM1_SSP585_V])
-    ukesm1_ssp585 = compute_metrics.MetricComputer(UKESM1_SSP585)
-    ukesm1_ssp585 = ukesm1_ssp585.subset(lat=slice(0, 90)) # , plev=50000)
-    # ukesm1_ssp585.get_available_metrics(all_metrics, return_coord_error=True)
-    
-    one_metric = 'Woolings2010' # metric_to_use
-    result = ukesm1_ssp585.compute_metric_from_data(one_metric, all_metrics=all_metrics, return_coord_error=False) # , subset_kwargs={'ignore_coords':['plev']
-    if result is not None:
-        max_lats = result[:,0]
-        max_ws = result[:,1]
+class TestPlot(unittest.TestCase):
+    def setUp(self):
+        UKESM1_SSP585_U = xr.open_dataset("data/ua_day_UKESM1-0-LL_ssp585_r2i1p1f2_gn_20150101-20491230.nc")
+        UKESM1_SSP585_V = xr.open_dataset("data/va_day_UKESM1-0-LL_ssp585_r2i1p1f2_gn_20150101-20491230.nc")
+        UKESM1_SSP585 = xr.merge([UKESM1_SSP585_U, UKESM1_SSP585_V])
+        ukesm1_ssp585 = compute_metrics.MetricComputer(UKESM1_SSP585)
+        self.data = ukesm1_ssp585.subset(lat=slice(0, 90))
+        self.all_metrics = JETSTREAM_METRIC_DICT
+
+    def plot_fig(self, save=False):
+        max_lats = self.result[:,0]
+        max_ws = self.result[:,1]
         fig, ax = plt.subplots(1)
         ax.plot(max_lats)
         ax.plot(max_ws)
         plt.legend(['latitude of max windspeed', 'windspeed'])
-        fig.savefig('experiments/figures/woolings_test.png', bbox_inches='tight')
-    print("done!")
+        if save:
+            fig.savefig('experiments/figures/woolings_test.png', bbox_inches='tight')
 
+    def test_plot(self): 
+        self.result = self.data.compute_metric_from_data('Woolings2010', all_metrics=self.all_metrics, return_coord_error=False)
+        self.assertIsInstance(self.result, np.array)
+        self.plot_fig()
+        self.plot_fig(save=True)
+        
+
+if __name__ == "__main__":
+    unittest.main()

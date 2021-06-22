f77258849860652a18ebecabfbb2c3b20a184c14
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-06-22 13:12:59 +0100

    add comment for later

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,1 +1,3 @@
 from . import compute_metrics, jetstream_metrics, jetstream_metrics_utils, jetstream_metrics_dict, windspeed_utils
+
+## this is where the metrics.[something] objects can go i.e. errors

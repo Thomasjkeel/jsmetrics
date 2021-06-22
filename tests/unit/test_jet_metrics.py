760a09721c2b89ac3d0261fa38e39492015f8634
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-06-22 14:02:36 +0100

    add jet metric tests

--- tests/unit/test_jet_metrics.py
+++ tests/unit/test_jet_metrics.py
@@ -19,36 +19,68 @@ __status__ = "Development"
 # @pytest.mark.parametrize(["metric_name, all_metrics", ("'Woolings2010', ALL_METRICS", "this works")]) # example TODO
 class TestKoch2006(unittest.TestCase):
     def test_basic(self):
+        pass
 
-class TestKoch2006(unittest.TestCase):
-    def test_basic(self):
 
-class TestKoch2006(unittest.TestCase):
+class TestArcherCaldeira2008(unittest.TestCase):
     def test_basic(self):
+        pass
 
-class TestKoch2006(unittest.TestCase):
-    def test_basic(self):
 
-class TestKoch2006(unittest.TestCase):
+class TestWoolings2010(unittest.TestCase):
     def test_basic(self):
+        pass
 
-class TestKoch2006(unittest.TestCase):
-    def test_basic(self):
 
-class TestKoch2006(unittest.TestCase):
+class TestManney2011(unittest.TestCase):
     def test_basic(self):
+        pass
 
-class TestKoch2006(unittest.TestCase):
-    def test_basic(self):
 
-class TestKoch2006(unittest.TestCase):
+class TestScreenSimmonds2013(unittest.TestCase):
     def test_basic(self):
+        pass
 
-class TestKoch2006(unittest.TestCase):
-    def test_basic(self):
 
-class TestKoch2006(unittest.TestCase):
+class TestKuang2014(unittest.TestCase):
     def test_basic(self):
+        pass
+
+
+class TestFrancisVavrus2015(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestLocalWaveActivity(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestCattiaux2016(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestCeppi2018(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestKern2018(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestSimpson2018(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+class TestChemkeMing2020(unittest.TestCase):
+    def test_basic(self):
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()

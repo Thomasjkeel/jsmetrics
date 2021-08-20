29df6056631ca6baf854502f60a3ae6b95f2b20a
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-08-20 19:45:18 +0100

    add v data script

---
+++ data/get_era5_v_data.py
@@ -0,0 +1,19 @@
+# requires configuration file: /home/[yourcomputer]/.cdsapirc
+import cdsapi
+
+c = cdsapi.Client()
+
+p_levels = [250, 300, 500, 700, 850, 925]
+
+c.retrieve("reanalysis-era5-pressure-levels", 
+    {
+    "variable":"v",
+    "date":"1979-01-01/2020-03-01",
+    "time": "00:00",
+    "pressure_level": p_levels,
+    "area": [90, 0, 0, 360],
+    "product_type": "reanalysis",
+    "format": 'netcdf',
+    "grid": "1.0/1.0"
+    },
+"era5_v_wind_79to21.nc")

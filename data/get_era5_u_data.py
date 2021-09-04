eb91d898846a31fe0464a56207450db689eb5cde
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-09-04 10:44:24 +0200

    update get data

--- data/get_era5_u_data.py
+++ data/get_era5_u_data.py
@@ -8,7 +8,7 @@ p_levels = [250, 300, 500, 700, 850, 925]
 c.retrieve("reanalysis-era5-pressure-levels", 
     {
     "variable":"u",
-    "date":"1979-01-01/2020-03-01",
+    "date":"1981-01-01/2021-07-01",
     "time": "00:00",
     "pressure_level": p_levels,
     "area": [90, 0, 0, 360],
@@ -16,4 +16,4 @@ c.retrieve("reanalysis-era5-pressure-levels",
     "format": 'netcdf',
     "grid": "1.0/1.0"
     },
-"era5_u_wind_79to21.nc")
+"era5_u_wind_81to21.nc")

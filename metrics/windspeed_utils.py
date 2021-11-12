a4901bb779d197f657e1a7c9ab1bb94b58887100
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-11-12 14:21:21 +0000

    pylint update 9

--- metrics/windspeed_utils.py
+++ metrics/windspeed_utils.py
@@ -35,58 +35,59 @@ class WindSpeedSlice():
         Base class for windspeed slice
     """
     def __init__(self, data, req_variables=('ua', 'va')):
-        ## these will check that the correct data variables and coords are available in the xarray data input
+        ## these will check that the correct variables/coords are available in the xarray input
         self.req_variables = req_variables
         self._check_input_data_can_be_used_for_windspeed_slice(data)
         self.values = self._calc_windspeed(data)
         self.values = self.values.rename('ws').to_dataset()
-    
+
     def __init_subclass__(cls, req_coords, *a, **kw):
         cls.req_coords = req_coords
 
     def __getitem__(self, item):
-         return self.values[item]
-    
+        return self.values[item]
+
     def __add__(self, other):
         if self.req_coords == other.req_coords and self.req_variables == other.req_variables:
             return xr.concat([self.values, other.values], dim='time')
-    
+
     @staticmethod
     def _calc_windspeed(data):
         """
-            The reason this exists is that their may be a point in time where constraitns on this calculation will need to be made i.e. if too much data or too much RAM in use
+            The reason this exists is that their may be a point in time where constraints
+            on this calculation will need to be made i.e. if too much data or too much RAM in use
         """
         return get_resultant_wind(data['ua'], data['va'])
-        
+
     def _check_input_data_can_be_used_for_windspeed_slice(self, data):
         check_if_xarray_dataset_or_array(data)
         check_coord_in_data(data, self.req_coords)
         check_var_in_data(data, self.req_variables)
         check_only_required_coords_are_in_data(data, self.req_coords, to_remove=('bnds',))
-    
+
     def label_slice(self, condition, label):
         return self.values.where(condition, other=label)
-    
+
     def get_values(self):
         return self.values
 
-        
+
 class PressureLevelWindSpeedSlice(WindSpeedSlice, req_coords=('lat', 'lon')):
     """
         Data will be lon*lat
-    """ 
-    def print_lats(self):
-        print(self['lat'])
-    
-        
-class LatitudeWindSpeedSlice(WindSpeedSlice, req_coords=('lat', 'plev')):
     """
-        Data will be lon*plev
-    """ 
     def print_lats(self):
         print(self['lat'])
 
-        
+
+class LatitudeWindSpeedSlice(WindSpeedSlice, req_coords=('lat', 'plev')):
+    """
+        Data will be lon*plev
+    """
+    def print_lats(self):
+        print(self['lat'])
+
+
 ## Checks for windspeed slices
 def check_if_xarray_dataset_or_array(data):
     if not isinstance(data, xr.Dataset) or  isinstance(data, xr.DataArray):
@@ -110,11 +111,13 @@ def check_only_required_coords_are_in_data(data, req_coords, to_remove=()):
         try:
             dims.remove(rem)
         except:
+            # TODO
             pass
             # print('cannot remove %s from dims' % (rem))
 
     req_coords = set(req_coords)
     difference = dims.difference(set(req_coords))
     if len(difference) > 0:
-        raise ValueError("Unwanted coords in data: %s. Please subset/remove so that the slice can be 2D." % (difference,))
+        raise ValueError("Unwanted coords in data: %s.\
+                 Please subset/remove so that the slice can be 2D." % (difference,))
     

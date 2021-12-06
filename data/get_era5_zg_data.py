45ff91ccd7b0d2adc7c89e7478eac86545e6e66d
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-12-06 17:33:28 +0000

    change format

--- data/get_era5_zg_data.py
+++ data/get_era5_zg_data.py
@@ -5,15 +5,17 @@ c = cdsapi.Client()
 
 p_levels = [500]
 
-c.retrieve("reanalysis-era5-pressure-levels", 
+c.retrieve(
+    "reanalysis-era5-pressure-levels",
     {
-    "variable":"z",
-    "date":"1979-01-01/2020-03-01",
-    "time": "00:00",
-    "pressure_level": p_levels,
-    "area": [90, 0, 0, 360],
-    "product_type": "reanalysis",
-    "format": 'netcdf',
-    "grid": "1.0/1.0"
+        "variable": "z",
+        "date": "1979-01-01/2020-03-01",
+        "time": "00:00",
+        "pressure_level": p_levels,
+        "area": [90, 0, 0, 360],
+        "product_type": "reanalysis",
+        "format": "netcdf",
+        "grid": "1.0/1.0",
     },
-"era5_zg_79to21.nc")
+    "era5_zg_79to21.nc",
+)

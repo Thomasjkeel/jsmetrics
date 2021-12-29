7a904b109b935dd018bdc554799d7204cb18d4ef
Thomasjkeel <thomasjames.keel@googlemail.com>
2021-12-29 18:13:33 +0000

    update init with version

--- jsmetrics/__init__.py
+++ jsmetrics/__init__.py
@@ -1,3 +1,9 @@
+"""Top-level package for jet-stream-metrics."""
+
+__author__ = """Tom Keel"""
+__email__ = "thomas.keel.18@ucl.ac.uk"
+__version__ = "0.1.0"
+
 # from . import (
 #     compute_metrics,
 #     general_utils,

3fe2cabc48e5d690f424749568b65f0598f8b694
Thomasjkeel <thomasjames.keel@googlemail.com>
2022-04-28 14:22:42 +0100

    update with 'plev_units' arg

--- jsmetrics/jetstream_metrics_dict.py
+++ jsmetrics/jetstream_metrics_dict.py
@@ -7,16 +7,19 @@ from . import jetstream_metrics
 # 3. 'coords' will contain the required CMIP6? standard coords
 #       and each coord will provide a list of 2 values: mininum value
 #       for coord and maximum value for coord and must be a number
-#    3.1 for 'plev' coord it is in millibars and higher pressure is considered
-#        the minimum value (e.g. 85000 mbar - 50000 mbar)
-# 4. 'metric' is the name of a function in jetstream_metrics.py
-# 5. 'name' is the paper name
-# 6. 'doi' is the DOI link
+#    3.1 for 'plev' coord it is in Pa/mbar and higher pressure is considered
+#        the minimum value (e.g. 85000 Pmbar - 50000 mbar)
+# 4. 'plev_units' is units for the pressure level
+# 5. 'metric' is the name of a function in jetstream_metrics.py
+# 6. 'name' is the paper name
+# 7. 'doi' is the DOI link
+
 
 JETSTREAM_METRIC_DICT = {
     "Koch2006": {
         "variables": ["ua", "va"],
         "coords": {"plev": [10000, 40000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.koch_et_al_2006,
         "name": "Koch et al. 2006",
         "description": "",
@@ -25,6 +28,7 @@ JETSTREAM_METRIC_DICT = {
     "ArcherCaldeira2008": {
         "variables": ["ua", "va"],
         "coords": {"plev": [10000, 40000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.archer_caldeira_2008,
         "name": "Archer & Caldeira 2008",
         "description": "",
@@ -33,6 +37,7 @@ JETSTREAM_METRIC_DICT = {
     "BartonEllis2009": {
         "variables": ["ua", "va"],
         "coords": {"plev": [30000, 30000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.barton_ellis_2009,
         "name": "Barton & Ellis 2009",
         "description": "",
@@ -41,6 +46,7 @@ JETSTREAM_METRIC_DICT = {
     "Schiemann2009": {
         "variables": ["ua", "va"],
         "coords": {"plev": [10000, 50000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.schiemann_et_al_2009,
         "name": "Schiemann et al 2009",
         "description": "",
@@ -49,6 +55,7 @@ JETSTREAM_METRIC_DICT = {
     "Woolings2010": {
         "variables": ["ua"],
         "coords": {"plev": [70000, 92500], "lat": [15, 75], "lon": [120, 180]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.woolings_et_al_2010,
         "name": "Woolings et al. 2010",
         "description": "",
@@ -57,6 +64,7 @@ JETSTREAM_METRIC_DICT = {
     "Manney2011": {
         "variables": ["ua", "va"],
         "coords": {"plev": [10000, 40000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.manney_et_al_2011,
         "name": "Manney et al. 2011",
         "description": "surface to 0.1 hPa",
@@ -65,6 +73,7 @@ JETSTREAM_METRIC_DICT = {
     "PenaOrtiz2013": {
         "variables": ["ua", "va"],
         "coords": {"plev": [10000, 40000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.penaortiz_et_al_2013,
         "name": "Pena-Ortiz et al. 2013",
         "description": "",
@@ -73,6 +82,7 @@ JETSTREAM_METRIC_DICT = {
     "ScreenSimmonds2013": {
         "variables": ["zg"],
         "coords": {"plev": [50000, 50000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.screen_and_simmonds_2013,
         "name": "Screen & Simmonds 2013",
         "description": "",
@@ -81,6 +91,7 @@ JETSTREAM_METRIC_DICT = {
     "Kuang2014": {
         "variables": ["ua", "va"],
         "coords": {"plev": [20000, 25000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.kuang_et_al_2014,
         "name": "Kuang et al. 2014.",
         "description": "NOTE: Should be 200hPa only",
@@ -89,6 +100,7 @@ JETSTREAM_METRIC_DICT = {
     "FrancisVavrus2015": {
         "variables": ["ua", "va"],
         "coords": {"plev": [50000, 50000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.francis_vavrus_2015,
         "name": "Francis & Vavrus 2015",
         "description": "",
@@ -102,6 +114,7 @@ JETSTREAM_METRIC_DICT = {
     "Cattiaux2016": {
         "variables": ["zg"],
         "coords": {"plev": [50000, 50000]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.cattiaux_et_al_2016,
         "name": "Cattiaux et al. 2016",
         "description": "",
@@ -110,6 +123,7 @@ JETSTREAM_METRIC_DICT = {
     "GrisePolvani2017": {
         "variables": ["ua"],
         "coords": {"plev": [85000, 85000], "lat": [-65, -30]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.grise_polvani_2017,
         "name": "Grise & Polvani 2017",
         "description": "",
@@ -122,6 +136,7 @@ JETSTREAM_METRIC_DICT = {
     "Bracegirdle2018": {
         "variables": ["ua"],
         "coords": {"plev": [85000, 85000], "lat": [-75, -10]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.bracegirdle_et_al_2018,
         "name": " Bracegirdle et al. 2018",
         "description": "",
@@ -130,25 +145,26 @@ JETSTREAM_METRIC_DICT = {
     "Ceppi2018": {
         "variables": ["ua"],
         "coords": {"plev": [85000, 85000], "lat": [30, 60]},
+        "plev_units": "Pa",
         "metric": jetstream_metrics.ceppi_et_al_2018,
         "name": "Ceppi et al. 2018",
         "description": "",
         "doi": "10.1175/JCLI-D-17-0323.1",
     },
     # "Kern2018":
-    #     {"variables": ["ua", "va"], "coords":{"plev": [0, 100000]},
+    #     {"variables": ["ua", "va"], "coords":{"plev": [0, 100000]}, "plev_units": "Pa",
     #      "metric": jetstream_metrics.kern_et_al_2018,
     #      "description": "Kern 2018"},
     # "Rikus2018":
-    #     {"variables": ["ua"], "coords":{"plev": [0, 100000]},
+    #     {"variables": ["ua"], "coords":{"plev": [0, 100000]}, "plev_units": "Pa",
     #      "metric": jetstream_metrics.rikus_2018,
     #      "description": "Rikus 2018"},
     # "Simpson2018":
-    #     {"variables": ["ua"], "coords":{"plev": [70000, 70000]},
+    #     {"variables": ["ua"], "coords":{"plev": [70000, 70000]}, "plev_units": "Pa",
     #      "metric": jetstream_metrics.simpson_et_al_2018,
     #      "description": "Simpson et al. 2018"},
     # "ChemkeMing2020":
-    #     {"variables": ["ua","va"], "coords":{"plev": [0, 100000]},
+    #     {"variables": ["ua","va"], "coords":{"plev": [0, 100000]}, "plev_units": "Pa",
     #      "metric": jetstream_metrics.chemke_and_ming_2020,
     #      "description": "Chemke & Ming 2020"}
 }

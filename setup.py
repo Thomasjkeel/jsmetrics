3e42a031143f8db3bd4cd005861f071cb3b492d3
Thomasjkeel <thomasjames.keel@googlemail.com>
2022-05-03 13:04:34 +0100

    update black and bumpo to version 0.0.5 beta

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/Thomasjkeel/jet-stream-metrics"
 AUTHOR = "Thomas Keel"
 AUTHOR_EMAIL = "thomas.keel.18@ucl.ac.uk"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.0.5-alpha"
+VERSION = "0.0.5-beta"
 LICENSE = "MIT License"
 
 KEYWORDS = "jet-stream metrics climate xarray"

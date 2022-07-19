04c8bce2323bf755da63b29f7854c2c05249746a
Thomasjkeel <thomasjames.keel@googlemail.com>
2022-07-19 13:48:13 +0100

    add alg components

---
+++ jsmetrics/jetstream_algorithms_components.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Algorithms and calculations for the metrics used to
+    identify or classify jet-stream in the literature.
+
+    This file is in order of publish year of the metrics
+    (see details_for_all_metrics.py)
+"""
+
+# imports
+# import collections
+# import numpy as np
+# import matplotlib.pyplot
+# import xarray as xr
+# import scipy.fftpack
+# import scipy.interpolate
+# # import shapely.geometry
+# from . import data_utils, spatial_utils, windspeed_utils
+
+# docs
+__author__ = "Thomas Keel"
+__email__ = "thomas.keel.18@ucl.ac.uk"
+__status__ = "Development"

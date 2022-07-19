051398b663a90ce29172ec32398b20b7413f656b
Thomasjkeel <thomasjames.keel@googlemail.com>
2022-07-19 15:54:42 +0100

    update local min maxima test

--- tests/unit/test_data_utils.py
+++ tests/unit/test_data_utils.py
@@ -40,10 +40,40 @@ class TestXarrayDataCheck(unittest.TestCase):
 
     def test_check_at_least_n_plevs_in_data(self):
         tested_func = data_utils.check_at_least_n_plevs_in_data
-        self.assertRaises(ValueError, lambda: tested_func(self.data))
+        tested_func(self.data, n_plevs=1)
+        self.assertRaises(
+            ValueError, lambda: tested_func(self.data, n_plevs=10)
+        )
 
     def test_check_if_data_is_xarray_datatype(self):
         tested_func = data_utils.check_if_data_is_xarray_datatype
         tested_func(self.data)
         new_data = self.data["ua"].data
         self.assertRaises(TypeError, lambda: tested_func(new_data))
+
+
+class TestLocalMinimaMaxima(unittest.TestCase):
+    def setUp(self):
+        self.data = set_up_test_uv_data()
+
+    def test_get_local_maxima(self):
+        tested_func = data_utils.get_local_maxima
+        self.assertListEqual(
+            list(
+                tested_func(self.data["ua"].isel(time=0, plev=0, lon=0).data)[
+                    0
+                ]
+            ),
+            [4, 30, 38, 44, 56, 61, 70],
+        )
+
+    def test_get_local_minima(self):
+        tested_func = data_utils.get_local_minima
+        self.assertListEqual(
+            list(
+                tested_func(self.data["ua"].isel(time=0, plev=0, lon=0).data)[
+                    0
+                ]
+            ),
+            [43, 48, 58, 65],
+        )

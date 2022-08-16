16bd3c88c057e487a70e1a62be2fe472dd592445
Thomasjkeel <thomasjames.keel@googlemail.com>
2022-08-16 16:14:08 +0100

    Bump version: 0.0.7-beta → 0.0.8-alpha

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/Thomasjkeel/jet-stream-metrics"
 AUTHOR = "Thomas Keel"
 AUTHOR_EMAIL = "thomas.keel.18@ucl.ac.uk"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.0.7-beta"
+VERSION = "0.0.8-alpha"
 LICENSE = "MIT License"
 
 KEYWORDS = "jet-stream climate metrics algorithms xarray"

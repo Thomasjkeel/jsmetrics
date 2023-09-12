b717bdc7bf4880598dec76cc4de25c5ff86f9aa2
Thomasjkeel <thomasjames.keel@googlemail.com>
2023-09-12 16:49:22 +0100

    Bump version: 0.1.6-beta → 0.1.6

--- jsmetrics/__init__.py
+++ jsmetrics/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tom Keel"""
 __email__ = "thomas.keel.18@ucl.ac.uk"
-__version__ = "0.1.6-beta"
+__version__ = "0.1.6"
 
 from . import details_for_all_metrics
 from .metrics import jet_core_algorithms, jet_statistics, waviness_metrics

a51dd234cfcbd64e4cc3cf0f6f03fb7c698d8c56
Thomasjkeel <thomasjames.keel@googlemail.com>
2024-09-24 16:18:05 +0100

    v0.2.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/Thomasjkeel/jsmetrics"
 AUTHOR = "Thomas Keel"
 AUTHOR_EMAIL = "thomas.keel.18@ucl.ac.uk"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.2.7-alpha"
+VERSION = "0.2.7-beta"
 LICENSE = "MIT License"
 
 KEYWORDS = "jet-stream climate metrics algorithms xarray"

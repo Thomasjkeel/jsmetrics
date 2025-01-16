c2f5b697be6b3f44376923d2ff1e0573d70ef70e
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-01-16 16:32:51 +0000

    manually update version to 0.29

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/Thomasjkeel/jsmetrics"
 AUTHOR = "Thomas Keel"
 AUTHOR_EMAIL = "thomasjames.keel@gmail.com"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.2.7"
+VERSION = "0.2.9"
 LICENSE = "MIT License"
 
 KEYWORDS = "jet-stream climate metrics algorithms xarray"

8672f4cbe4a42ae78c97378d61f916993fbde0d0
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-01-16 16:17:44 +0000

    add check for numpy array

--- jsmetrics/utils/spatial_utils.py
+++ jsmetrics/utils/spatial_utils.py
@@ -289,11 +289,9 @@ def get_one_contour_linestring(dataarray, contour_level):
     ), "Data array needs to have latitude and longitude coords"
     if "time" in dataarray.dims:
         dataarray = dataarray.squeeze("time")
-    try:
-        assert contour_level == False
-    except:
-        raise AssertionError(f"FAILED: {type(contour_level)}")
-    one_contour = dataarray.plot.contour(levels=[float(contour_level[0])])
+    if not isinstance(contour_level, np.ndarray):
+        contour_level = np.array(contour_level)
+    one_contour = dataarray.plot.contour(levels=[float(contour_level)])
     matplotlib.pyplot.close()
     one_contour_segments = seperate_one_contour_into_line_segments(
         one_contour.get_paths()[0]

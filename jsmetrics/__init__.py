c980b2085307f6b5c465ab9a57285dca8c2c092b
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-11-03 12:01:11 +0000

    remove os and sys

--- jsmetrics/__init__.py
+++ jsmetrics/__init__.py
@@ -4,5 +4,17 @@ __author__ = """Tom Keel"""
 __email__ = "thomasjames.keel@gmail.com"
 __version__ = "0.2.9"
 
-from . import details_for_all_metrics
-from .metrics import jet_core_algorithms, jet_statistics, waviness_metrics
+from jsmetrics import details_for_all_metrics
+from jsmetrics.metrics import (
+    jet_core_algorithms,
+    jet_statistics,
+    waviness_metrics,
+)
+
+
+__all__ = [
+    "details_for_all_metrics",
+    "jet_core_algorithms",
+    "jet_statistics",
+    "waviness_metrics",
+]

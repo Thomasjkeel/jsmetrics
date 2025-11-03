4f32ba84eab861a0021253dd80896743c181f26f
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-11-03 12:05:08 +0000

    Bump version: 0.2.9 → 0.3.0-alpha

--- jsmetrics/__init__.py
+++ jsmetrics/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tom Keel"""
 __email__ = "thomasjames.keel@gmail.com"
-__version__ = "0.2.9"
+__version__ = "0.3.0-alpha"
 
 from jsmetrics import details_for_all_metrics
 from jsmetrics.metrics import (
